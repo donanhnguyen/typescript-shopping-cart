7220645e636580ad749eae7facf20c013e324950
thedonwind69@gmail.com <thedonwind69@gmail.com>
2023-05-22 15:17:36 -0700

    nabar stuff

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,5 @@
-import { Container, Navbar } from "react-bootstrap"
+import { Container, Nav, Navbar } from "react-bootstrap"
+import { NavLink } from "react-router-dom"
 
 export function NavBar () {
 
@@ -6,6 +7,12 @@ export function NavBar () {
     return (
         <Navbar className="bg-white shadow-sm mb-3">
             <Container>
+                <Nav>
+                    <Nav.Link as={NavLink} to={'/'}>Home</Nav.Link>
+                    <Nav.Link as={NavLink} to={'/about'}>About</Nav.Link>
+                    <Nav.Link as={NavLink} to={'/store'}>Store</Nav.Link>
+
+                </Nav>
 
             </Container>
         </Navbar>
