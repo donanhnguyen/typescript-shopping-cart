258ef50ae42457c63ba8f210e4a0223d60b13fdb
thedonwind69@gmail.com <thedonwind69@gmail.com>
2023-05-22 15:21:07 -0700

    nabar stuff

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Container, Nav, Navbar } from "react-bootstrap"
+import { Container, Nav, Navbar, Button} from "react-bootstrap"
 import { NavLink } from "react-router-dom"
 
 export function NavBar () {
@@ -7,12 +7,15 @@ export function NavBar () {
     return (
         <Navbar className="bg-white shadow-sm mb-3">
             <Container>
-                <Nav>
+                <Nav className="me-auto">
                     <Nav.Link as={NavLink} to={'/'}>Home</Nav.Link>
                     <Nav.Link as={NavLink} to={'/about'}>About</Nav.Link>
                     <Nav.Link as={NavLink} to={'/store'}>Store</Nav.Link>
 
                 </Nav>
+
+
+                <Button style={{width: '3rem', height: '3rem'}}></Button>
 
             </Container>
         </Navbar>
